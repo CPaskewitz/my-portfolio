77aa5ffbc7ba2cb2da1019fa2bf081f14f3d9b36
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-06-24 15:37:33 -0400

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import {Navbar} from './components/Navbar/Navbar';
-import {Hero} from './components/Hero/Hero';
-import {About} from './components/About/About';
-import {Projects} from './components/Projects/Projects';
-import {Contact} from './components/Contact/Contact';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 import './App.css';
 
 const App: React.FC = () => {
