e75d52095426aec59403e13089b0fd68115ca832
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-06-24 15:31:53 -0400

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import './App.css'
-import { Navbar} from './components/Navbar/Navbar'
+import React, { useEffect, useState } from 'react';
+import {Navbar} from './components/Navbar/Navbar';
+import {Hero} from './components/Hero/Hero';
+import {About} from './components/About/About';
+import {Projects} from './components/Projects/Projects';
+import {Contact} from './components/Contact/Contact';
+import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > window.innerHeight) {
+      setIsNavbarVisible(true);
+    } else {
+      setIsNavbarVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <div>
+      {isNavbarVisible && <Navbar />}
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
