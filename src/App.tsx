3204c0680759edc489f54589a2fb6ec7a4127906
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-06-24 15:59:15 -0400

    fix app styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
-import './App.css';
+import './app.scss';
 
 const App: React.FC = () => {
   const [isNavbarVisible, setIsNavbarVisible] = useState(false);
