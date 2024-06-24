94e85033e8dfae9aad451950bdf75b7de6e96ec1
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-06-24 15:29:47 -0400

    remove react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>,
 )
