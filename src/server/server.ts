353e09c91e2ed6d2bd34e5aaede956a3b8c01fed
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-06-27 13:22:03 -0400

    fix configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,10 +4,10 @@ import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../../dist')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('*', (_req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
 });
 
 app.listen(PORT, () => {
