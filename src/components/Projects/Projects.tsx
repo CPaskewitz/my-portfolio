e126677723a98988205dcfbb634ff1e545537872
Corey Paskewitz <corey.paskewitz@gmail.com>
2025-03-19 16:48:21 -0400

    add project live link

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -19,89 +19,89 @@ interface Project {
 
 const Projects: React.FC = () => {
     const projects: Project[] = [
-        {
-            title: 'BoguStore',
-            description: 'A mock e-commerce store showcasing unit testing.',
-            images: [
-                '/Bogustore_Home_Page.png',
-                '/Bogustore_Category_Page.png',
-                '/Bogustore_Sale_Page.png',
-                '/Bogustore_Cart_Page.png',
-                '/Bogustore_Shipping_Info.png',
-                '/Bogustore_Payment_Info.png',
-                '/Bogustore_Order_Summary.png',
-                '/bogustore_productlist_test.png',
-                '/bogustore_cart_test.png',
-                '/bogustore_checkout_test.png'
-            ],
-            link: '',
-            github: 'https://github.com/CPaskewitz/bogustore',
-            skills: [
-                { image: 'nextjsicon.png', name: 'Next.js' },
-                { image: 'typescripticon.png', name: 'TypeScript' },
-                { image: 'reduxicon.png', name: 'Redux' },
-                { image: 'tailwindicon.png', name: 'Tailwind' },
-                { image: 'jesticon.png', name: 'Jest' },
-            ]
-        },
-        {
-            title: 'QuestListRPG',
-            description: 'A task manager app with RPG elements.',
-            images: [
-                '/QuestListRPG_Home_Page.png',
-                '/QuestListRPG_Login.png',
-                '/QuestListRPG_Levelup.png',
-                '/QuestListRPG_Shop.png',
-                '/QuestListRPG_Stats.png',
-            ],
-            link: '',
-            github: 'https://github.com/CPaskewitz/taskmasterrpg',
-            skills: [
-                { image: 'mongodbicon.png', name: 'MongoDB' },
-                { image: 'reacticon.png', name: 'React' },
-                { image: 'typescripticon.png', name: 'TypeScript' },
-                { image: 'restapiicon.png', name: 'Dall-E 3 API' },
-                { image: 'herokuicon.png', name: 'Heroku' }
-            ]
-        },
-        {
-            title: 'PetAdoptr',
-            description: 'A mobile app for finding animals available for adoption.',
-            images: [
-                '/PetAdoptr_Grid_Page.jpeg',
-                '/PetAdoptr_Pet_Profile.jpeg',
-                '/PetAdoptr_DM.jpeg',
-                '/PetAdoptr_LM.jpeg',
-                '/PetAdoptr_Web_Sockets.jpeg',
-                '/PetAdoptr_Knex.jpeg',
-            ],
-            link: '',
-            github: '',
-            skills: [
-                { image: 'mysqlicon.png', name: 'MySQL' },
-                { image: 'reacticon.png', name: 'React Native' },
-                { image: 'nodeicon.png', name: 'Node.js' },
-                { image: 'typescripticon.png', name: 'TypeScript' },
-            ]
-        },
-        {
-            title: 'Leap Into Lessons',
-            description: 'A wordpress booking site for local summer swim lessons.',
-            images: [
-                'leapintolessons_hero.png',
-                'leapintolessons_map.png',
-                'leapintolessons_coaches.png',
-                'leapintolessons_faq.png',
-                'leapintolessons_booking.png',
-            ],
-            link: 'https://leapintolessons.com',
-            github: '',
-            skills: [
-                { image: 'wordpressicon.png', name: 'WordPress' },
-                { image: 'phpicon.png', name: 'PHP' },
-                { image: 'restapiicon.png', name: 'Google Maps API' }
-            ]
-        },
+      {
+        title: "BoguStore",
+        description: "A mock e-commerce store showcasing unit testing.",
+        images: [
+          "/Bogustore_Home_Page.png",
+          "/Bogustore_Category_Page.png",
+          "/Bogustore_Sale_Page.png",
+          "/Bogustore_Cart_Page.png",
+          "/Bogustore_Shipping_Info.png",
+          "/Bogustore_Payment_Info.png",
+          "/Bogustore_Order_Summary.png",
+          "/bogustore_productlist_test.png",
+          "/bogustore_cart_test.png",
+          "/bogustore_checkout_test.png",
+        ],
+        link: "https://bogustore.vercel.app/",
+        github: "https://github.com/CPaskewitz/bogustore",
+        skills: [
+          { image: "nextjsicon.png", name: "Next.js" },
+          { image: "typescripticon.png", name: "TypeScript" },
+          { image: "reduxicon.png", name: "Redux" },
+          { image: "tailwindicon.png", name: "Tailwind" },
+          { image: "jesticon.png", name: "Jest" },
+        ],
+      },
+      {
+        title: "QuestListRPG",
+        description: "A task manager app with RPG elements.",
+        images: [
+          "/QuestListRPG_Home_Page.png",
+          "/QuestListRPG_Login.png",
+          "/QuestListRPG_Levelup.png",
+          "/QuestListRPG_Shop.png",
+          "/QuestListRPG_Stats.png",
+        ],
+        link: "",
+        github: "https://github.com/CPaskewitz/taskmasterrpg",
+        skills: [
+          { image: "mongodbicon.png", name: "MongoDB" },
+          { image: "reacticon.png", name: "React" },
+          { image: "typescripticon.png", name: "TypeScript" },
+          { image: "restapiicon.png", name: "Dall-E 3 API" },
+          { image: "herokuicon.png", name: "Heroku" },
+        ],
+      },
+      {
+        title: "PetAdoptr",
+        description: "A mobile app for finding animals available for adoption.",
+        images: [
+          "/PetAdoptr_Grid_Page.jpeg",
+          "/PetAdoptr_Pet_Profile.jpeg",
+          "/PetAdoptr_DM.jpeg",
+          "/PetAdoptr_LM.jpeg",
+          "/PetAdoptr_Web_Sockets.jpeg",
+          "/PetAdoptr_Knex.jpeg",
+        ],
+        link: "",
+        github: "",
+        skills: [
+          { image: "mysqlicon.png", name: "MySQL" },
+          { image: "reacticon.png", name: "React Native" },
+          { image: "nodeicon.png", name: "Node.js" },
+          { image: "typescripticon.png", name: "TypeScript" },
+        ],
+      },
+      {
+        title: "Leap Into Lessons",
+        description: "A wordpress booking site for local summer swim lessons.",
+        images: [
+          "leapintolessons_hero.png",
+          "leapintolessons_map.png",
+          "leapintolessons_coaches.png",
+          "leapintolessons_faq.png",
+          "leapintolessons_booking.png",
+        ],
+        link: "https://leapintolessons.com",
+        github: "",
+        skills: [
+          { image: "wordpressicon.png", name: "WordPress" },
+          { image: "phpicon.png", name: "PHP" },
+          { image: "restapiicon.png", name: "Google Maps API" },
+        ],
+      },
     ];
 
     const [currentImageIndex, setCurrentImageIndex] = useState<number[]>(projects.map(() => 0));
